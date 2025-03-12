7220782369fa66b83370cffe4f12fbc52f0a74e6
hilal semiz <hilalsemiz87@gmail.com>
2025-03-12 10:05:50 +0300

    hilal

--- BookRentalWithoudDB/Controllers/StudentController.cs
+++ BookRentalWithoudDB/Controllers/StudentController.cs
@@ -41,5 +41,58 @@ namespace BookRentalWithoudDB.Controllers
             respository.Insert(student);
             return RedirectToAction("Index");
         }
+        public IActionResult Edit(int id)
+        {
+            StudentRespository repository = new StudentRespository();
+            var student = repository.GetStudent(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Student student)
+        {
+            StudentRespository repository = new StudentRespository();
+            var existingStudent = repository.GetStudent(id);
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+
+            existingStudent.Name = student.Name;
+            existingStudent.Surname = student.Surname;
+            existingStudent.BirthDate = student.BirthDate;
+
+            repository.Update(existingStudent);  // Veritabanında güncelleme işlemi
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            StudentRespository repository = new StudentRespository();
+            var student = repository.GetStudent(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            StudentRespository repository = new StudentRespository();
+            var student = repository.GetStudent(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            repository.Delete(id);  // Veritabanından öğrenci silme işlemi
+            return RedirectToAction("Index");
+        }
     }
 }
